1f304d041f4eb1a9da12e2e45c8f098f52777896
Daniel Koch <koch@triple6.org>
2014-12-15 16:45:15 +0100

    + slim & haml example

--- hello-world.rb
+++ hello-world.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
+require 'haml'
+require 'slim'
 
 class HelloWorld < Sinatra::Base
   get '/' do
     return "Hello World!"
+  end
+
+  get '/haml' do
+    haml :hello
+  end
+
+  get '/slim' do
+    slim :hello
   end
 end
